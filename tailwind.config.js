a7c5b6ed17899d654575ff708baa0ad390b0ec45
Alex Koles <alexandr.kolesnikov@gmail.com>
2022-01-24 23:20:36 +0200

    DEX token farm

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
     content: ["./src/**/*.{html,js}", "./index.html"],
     theme: {
-        extend: {},
+        extend: {
+            gridTemplateColumns: {
+                // Simple 16 column grid
+                '13': 'repeat(13, minmax(0, 1fr))',
+                '14': 'repeat(14, minmax(0, 1fr))',
+                '15': 'repeat(15, minmax(0, 1fr))',
+                '16': 'repeat(16, minmax(0, 1fr))',
+            }
+        },
     },
     plugins: [],
 }
