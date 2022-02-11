139d55668f93ff06163f17b434a172686e5a2b9f
mhladchenko <Deleting Bank Accounts>
2022-02-11 10:16:38 +0200

    fix main net button

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
                 '14': 'repeat(14, minmax(0, 1fr))',
                 '15': 'repeat(15, minmax(0, 1fr))',
                 '16': 'repeat(16, minmax(0, 1fr))',
+            },
+            screens: {
+                '3xl': '1800px',
             }
         },
     },
